990a3f677481ee9834d16621bca03f03f2cc795a
viktekbl <77540645+Vikek@users.noreply.github.com>
2024-01-07 18:37:32 +0100

    gh-pages publishing and update README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-app
+
+## weather-app that gets the weather for a location of your choice
+This project was built with weatherAPI with the purpose of practicing using API's after learning about them in the odin project.
+You can use the search-bar to get weather information for the location entered.
+
+[Live demo](https://vikek.github.io/weather-app/)
--- src/modules/getWeatherData.js
+++ src/modules/getWeatherData.js
@@ -1,5 +1,5 @@
 export default async function getWeatherData(location) {
-    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=e6b956b6bc604d8a842142951231511&days=3&q=' + location , {mode: 'cors'});
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=e6b956b6bc604d8a842142951231511&days=3&q=' + location , {mode: 'cors'});
     const data = await response.json();
     return data;
 }
